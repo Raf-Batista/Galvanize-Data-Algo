eb48196207b1fd05b11ec62bf4f33471f84661c7
Raf-Batista <RafaelB2424@gmail.com>
2021-05-18 16:21:04 -0400

    Implement quicksort

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class QuickSort {
+    /*
+        Quicksort implemented with ArrayLists
+
+        Runtime O(n log(n)) Average O(n^2) Worstcase.memory O(n log(n))
+    */
+    public static void quicksort(ArrayList<Integer> array) {
+        quicksort(array, 0, array.size() -1);
+    }
+
+    public static void quicksort(ArrayList<Integer> array, int left, int right) {
+        if (left >= right) return;
+
+        int pivot = array.get((left + right) /2);
+        int index = partition(array, left, right, pivot);
+        quicksort(array, left, index - 1);
+        quicksort(array, index, right);
+    }
+
+    public static int partition(ArrayList<Integer> array, int left, int right, int pivot) {
+        while (left <= right) {
+            while (array.get(left) < pivot) { left++; }
+            while (array.get(right) > pivot) { right--; }
+
+            if (left <= right) {
+                Collections.swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+}
---
+++ src/test/java/QuickSortTests.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QuickSortTests {
+
+    @Test
+    @DisplayName("It should return a sorted array")
+    public void quickSort_withUnsortedArrayOfInt() {
+        ArrayList<Integer> unsortedArray = new ArrayList<>();
+        unsortedArray.add(6);
+        unsortedArray.add(1);
+        unsortedArray.add(8);
+        unsortedArray.add(22);
+        unsortedArray.add(3);
+
+        ArrayList<Integer> sortedArray = new ArrayList<>();
+        sortedArray.add(1);
+        sortedArray.add(3);
+        sortedArray.add(6);
+        sortedArray.add(8);
+        sortedArray.add(22);
+
+        QuickSort.quicksort(unsortedArray, 0, 4);
+
+        assertEquals(sortedArray, unsortedArray);
+    }
+}
